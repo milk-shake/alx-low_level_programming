42f829b1426b4e29779f5da6470781b2daad5acc
Johnstone Gikenye <joegikenye@gmail.com>
2022-07-04 09:00:15 -0700

    alx-low_level_programming 0-preprocessor

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - entry point 
+ *
+ * Return: always returns a 0
+ */
+int main(void)
+{
+char a;
+for (a = 'a' ; a <= 'z' ; a++)
+purchar(a);
+purchar('\n');
+return (0);
+}
