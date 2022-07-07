10f5dbe96e06a816786b091a7b95ab24889a1c06
Njuguna Gikenye <joegikenye@gmail.com>
2022-07-07 10:17:42 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character
+ * @c:is the character that will use for the aurgument of the
+ * function
+ *
+ * Return : 1
+ */
+int _isupper(int c)
+{
+
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+		return (0);
+	printf("\n");
+}
+
