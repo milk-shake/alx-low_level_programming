e4b4773c62adb2ff8df4133e8433f15785489840
milk-shake <108178926+milk-shake@users.noreply.github.com>
2022-07-16 01:33:44 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,16 +9,16 @@
 int main(void)
 {
 	int i;
-	long int fibonacci[50];
+	long int fibonacci[98];
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
 	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
 
-	for (i = 2; i < 50; i++)
+	for (i = 2; i < 98; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		if (i == 49)
+		if (i == 97)
 			printf("%ld\n", fibonacci[i]);
 		else
 			printf("%ld, ", fibonacci[i]);
