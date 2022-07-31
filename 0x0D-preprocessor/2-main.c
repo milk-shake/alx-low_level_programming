deec1574ed2cfc035c2c3e8000c7cc5ed4a9beb5
joe <joegikenye@gmail.com>
2022-07-31 02:37:11 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#define _FILE_
+#include "4-sum.h"
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main entry point
+ * Return: the name of the file it was compiled from, followed
+ * by a new line
+ */
+int main(void)
+{
+	_putchar(_FILE_ '\n');
+	return (0);
+}
