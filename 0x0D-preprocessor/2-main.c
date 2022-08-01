cdbf415b2986528c238ca14e25f8842fd04ef54a
milk-shake <108178926+milk-shake@users.noreply.github.com>
2022-08-01 19:54:59 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,11 @@
-#define _FILE_
-#include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - main entry point
- * Return: the name of the file it was compiled from, followed
- * by a new line
+ *
+ * Return: name of the current program 
  */
 int main(void)
 {
-        _putchar(_FILE_ '\n');
-	return (0);
+printf("%s\n", _FILE_);
+return (0);
 }
