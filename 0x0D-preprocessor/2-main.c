0301dce19cc79ef4ad5a6079ec6313ceae9de414
milk-shake <108178926+milk-shake@users.noreply.github.com>
2022-08-01 19:42:04 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,24 +1,14 @@
-/**
 #define _FILE_
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
  * main - main entry point
  * Return: the name of the file it was compiled from, followed
  * by a new line
- 
+ */
 int main(void)
 {
-        printf("%s\n", _FILE_);
+        _putchar(_FILE_ '\n');
 	return (0);
-}
-*/
-#include <stdio.h>
-
-int main ()
-{
-    printf ("This is line %d of file \"%s\".\n",
-            __LINE__, __FILE__);
-    return 0;
 }
