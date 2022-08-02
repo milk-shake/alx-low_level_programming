5b7693fcd98c45498b9ffec5aecbc6ec420e7608
milk-shake <108178926+milk-shake@users.noreply.github.com>
2022-08-02 15:33:28 +0300

    fixed the free(dog->name)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -35,7 +35,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = malloc(n2 * sizeof(char));
 	if (dog->owner == NULL)
 	{
-		free(dog->name)
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
