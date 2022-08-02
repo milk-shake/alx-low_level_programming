2dee560e8317ddb97e21bf2d92b8e0cc8018b66e
milk-shake <108178926+milk-shake@users.noreply.github.com>
2022-08-02 15:11:59 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+
 /**
- * struct dog - struct dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * struct dog - information about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
 struct dog
 {
@@ -12,16 +13,16 @@ struct dog
 	float age;
 	char *owner;
 };
+
 /**
- * dog_t - typedef for struct dog
- * free - function tht frees dog
+ * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
 
 int _putchar(char);
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free(dog_t *d)
-#endif
+void free_dog(dog_t *d);
+
+#endif /* _DOG_H_ */
