44dec605cbabeccaf976ac21a6973a463a6a590c
milk-shake <108178926+milk-shake@users.noreply.github.com>
2022-08-02 15:20:35 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,21 +11,13 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name != NULL)
-		{
 			printf("Name :%s\n", d->name);
-		}
 		else
-		{
 			printf("Name :(nill)\n");
-		}
-		if (d->owner != NULL)
-		{
-			printf("Owner is :%s\n", d->owner);
-		}
-		else
-		{
-			printf("Owner is : (nil)\n");
-		}
 		printf("Age is : %.1f\n", d->age);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
 	}
 }
